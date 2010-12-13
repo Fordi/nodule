894bfc10ce16deccfc4be1053f898e882002fc45
elliottb2 <elliottb@trueaction.com>
2010-12-13 14:42:28 -0500

    Adding 500 error handling and verbose logging

--- modules/nodule/lib/dispatcher.js
+++ modules/nodule/lib/dispatcher.js
@@ -1,5 +1,13 @@
 module.exports = (function () {
-	var serverRoot = process.cwd(),
+	var httpError = function (Response, errorCode, errorMessage, errorTemplate, obj) {
+			Response.writeHead(errorCode, {'Content-Type': 'text/html'});
+			console.error('HTTP '+errorCode+': '+errorMessage);
+			console.error(obj);
+			obj.errorCode = errorCode;
+			obj.errorMessage = errorMessage;			
+			Response.end(require('./template')('Error/'+errorTemplate, obj));
+		},
+		serverRoot = process.cwd(),
 		conf = require(serverRoot+'/config.js'),
 		ModelBase = require('./model')(conf.datasource),
 		Url = require('url'),
@@ -14,22 +22,47 @@ module.exports = (function () {
 		},
 		dispatcher = null,
 		httpServer = function (req, res) {
+			console.log('---Request: '+req.url);
 			var url = Url.parse(req.url, true),
 				command = splitPath(url.pathname);
 			while (command.length<2) command.push('index');
+			var controllerName=command.shift(),
+				action=command.shift(),
+				ctl=null;
+			command.unshift(req, res, url.query);
 			try {
-				var ctl = require(serverRoot+'/Controller/'+command.shift()+'.js')(ModelBase),
-					action = command.shift();
-				var args = command.unshift(req, res, url.query);
-				if (ctl[action] && ctl[action].apply(dispatcher, args)) return;
-			} catch (e) {};
+				ctl = require(serverRoot+'/Controller/'+controllerName+'.js');
+			} catch(e) {};
+			if (ctl) {
+				try {
+					ctl=ctl(ModelBase);
+				} catch (e) {
+					console.error('Could not attach model base class to controller');
+					console.error('Failed with message:');
+					console.error(e.message);
+				}
+				if (ctl[action]) {
+					console.log(
+						'Serving request to controller: '+
+						controllerName+'::'+action
+					);
+					try {
+						ctl[action].apply(dispatcher, command);
+					} catch (e) {
+						console.error('Execution of '+controllerName+'::'+action+' failed');
+						httpError(res, 500, 'Programmers are bad at their job', 'General', {
+							url: req.url
+						});
+					}
+					return;
+				}
+			}
+			console.log('Trying to serve '+req.url);
 			StaticFile(req, res, function () {
-				res.writeHead(404, {'Content-Type': 'text/html'});
-				res.end(require('./template')('Error/General', {
-					errorCode: '404',
-					errorMessage: 'File Not Found',
+				console.log('File not Found: '+serverRoot+'/Static'+req.url);
+				httpError(res, 404, 'File Not Found', 'General', {
 					url: req.url
-				}));
+				});
 			});
 		};
 	dispatcher = require('http').createServer(httpServer);
