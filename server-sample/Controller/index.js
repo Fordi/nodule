5eb2eb9c1f075cff4fae87e04e4966dd06b96077
elliottb2 <elliottb@trueaction.com>
2010-12-13 14:43:23 -0500

    Adding failure test for generating 500 errors

--- server-sample/Controller/index.js
+++ server-sample/Controller/index.js
@@ -1,9 +1,14 @@
 module.exports = (function (Model) {
-	var Template = require('nodule/template');
+	var Template = require('nodule/lib/template');
 	return {
 		index: function (Request, Response, paramOptions) {
 			var Posts = Model('post');
+			Response.writeHead(404, {'Content-Type': 'text/html'});
+			Response.end('Nothing doing');
 			return true;
+		},
+		fail: function (Request, Response, paramOptions) {
+			this=5;
 		}
 	};
 });
